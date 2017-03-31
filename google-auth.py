0753a46a6dd284371ad8e8547585a24c64c9cc4c
Alexey.Ponimash <Alexey.Ponimash@gmail.com>
2017-03-31 17:19:59 +0300

    Track recording from OWNTRacks Sync dir with Google Drive

---
+++ google-auth.py
@@ -0,0 +1,43 @@
+import logging
+import argparse
+import httplib2
+from apiclient import discovery
+from apiclient.http import MediaIoBaseUpload
+import oauth2client.client
+import oauth2client.file
+import sys
+
+
+class LoadFromFile( argparse.Action ):
+    def __call__ (self, parser, namespace, values, option_string = None):
+       with values as f:
+           parser.parse_args(f.read().split(), namespace)
+
+parser = argparse.ArgumentParser()
+parser.add_argument( "--storage", default="googleapi.token" )
+parser.add_argument( "--client_id", required=True )
+parser.add_argument( "--secret", required=True )
+parser.add_argument( "--scope", nargs="+" )
+
+parser.add_argument( "--authcode", default=None )
+
+parser.add_argument( "-c", "--config", type=open, action=LoadFromFile, help="Load config from file" )
+parser.add_argument( "-v", action="store_true", default=False, help="Verbose logging", dest="verbose" )
+parser.add_argument( "--logfile", help="Logging into file" )
+args = parser.parse_args()
+logging.basicConfig(format="[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s",  level= logging.DEBUG if args.verbose else logging.INFO, filename=args.logfile )
+
+storage = oauth2client.file.Storage( args.storage )
+
+print args.scope
+flow = oauth2client.client.OAuth2WebServerFlow( args.client_id, args.secret, args.scope, redirect_uri="http://localhost" )
+
+if args.authcode==None:
+   auth_url = flow.step1_get_authorize_url()
+   logging.error("Open browser at %s and re-run with --authcode= argument", auth_url)
+   sys.exit()
+
+else:
+   credentials = flow.step2_exchange( args.authcode )
+   storage.put(credentials)
+   logging.info("Google auth token stored in %s", args.storage )
---
+++ trackrec.py
@@ -0,0 +1,83 @@
+import logging
+import argparse
+import urlparse
+import paho.mqtt.client as mqtt
+import json
+import os.path
+import gpxpy.gpx
+
+class GPXRecorder:
+      def __init__(self, url, storage ):
+          self.url = url
+          self.mqttc = mqtt.Client()
+          if url.username!=None:
+              self.mqttc.username_pw_set( url.username, url.password )
+          self.mqttc.on_connect = self.on_mqtt_connect
+          self.mqttc.on_message = self.on_mqtt_message
+          self.storage = storage
+
+      def start(self):
+          self.mqttc.connect( self.url.hostname, self.url.port if self.url.port!=None else 1883, 60 )
+          # mqtt broker
+          logging.info("Trying connect to MQTT broker at %s:%d" % (self.url.hostname, self.url.port) )
+          self.mqttc.loop_forever()
+ 
+
+      def on_mqtt_connect(self, client, userdata, flags, rc):
+          logging.info("MQTT broker connection result: %s", mqtt.connack_string(rc) )
+          if rc==0:
+             client.subscribe("owntracks/+/+/track", 0)
+          pass
+
+      def on_mqtt_message(self, client, userdata, message):
+          logging.info("Got mqtt message: %s %s", message.topic, message.payload )
+          d = message.topic.split("/")
+          device, tracker = (d[1],d[2])
+
+          data = json.loads(message.payload)
+          if "track" in data:
+             gpx = self.track2gpx( data["track"] )
+
+             start_time = gpx.get_time_bounds().start_time
+             filename = "%s-%s.gpx" % (device, start_time.strftime("%Y_%m_%d-%H_%M") )
+             logging.info("Storing track to %s", filename)
+
+             f = open( os.path.append(self.storage, filename), "wb" )
+             f.write( gpx.to_xml() )
+             f.close()
+          pass
+
+      def track2gpx(self, track):
+          gpx = gpxpy.gpx.GPX()
+          gpx_track = gpxpy.gpx.GPXTrack()
+          gpx.tracks.append(gpx_track)
+          gpx_segment = gpxpy.gpx.GPXTrackSegment()
+          gpx_track.segments.append(gpx_segment)
+
+          for idx, p in enumerate(track):
+             point = gpxpy.gpx.GPXTrackPoint(
+                  p["lat"], p["lon"], elevation= p['alt'] if 'alt' in p else None, time=datetime.datetime.fromtimestamp( p["tst"] ), speed=p["vel"] if 'vel' in p else None,
+                  name = "Start" if idx==0 else ( "Finish" if idx==len(track)-1 else None )
+             )
+             gpx_segment.points.append( point )
+          return gpx
+
+
+if __name__ == "__main__":
+   class LoadFromFile( argparse.Action ):
+       def __call__ (self, parser, namespace, values, option_string = None):
+          with values as f:
+              parser.parse_args(f.read().split(), namespace)
+
+   parser = argparse.ArgumentParser()
+   parser.add_argument( "--storage", default="googleapi.token" )
+
+   parser.add_argument( "-c", "--config", type=open, action=LoadFromFile, help="Load config from file" )
+   parser.add_argument( "-u","--url", default="localhost:1883", type=urlparse.urlparse )
+   parser.add_argument( "-v", action="store_true", default=False, help="Verbose logging", dest="verbose" )
+   parser.add_argument( "--logfile", help="Logging into file" )
+   args = parser.parse_args()
+   logging.basicConfig(format="[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s",  level= logging.DEBUG if args.verbose else logging.INFO, filename=args.logfile )
+
+   recorder = GPXRecorder( args.url, storage=args.storage )
+   recorder.start()
